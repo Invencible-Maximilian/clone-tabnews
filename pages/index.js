e4384af9464959fbacba3666162e1c8d4daebbad
Invencible-Maximilian <Maximilianwachter@outlook.com>
2026-01-17 13:06:45 +0000

    Adicionando o lint:check e lint:fix do prettier

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-function Home(){
-    return<h1>Alterando a frase do meu h1</h1>
+function Home() {
+  return <h1>Alterando a frase do meu h1</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("Identação errada");
 }
 
 export default Home;
-    
