a9dc20fb4e51ab6a0a435e16e2c2e9303f81ba3b
Invencible-Maximilian <Maximilianwachter@outlook.com>
2026-02-06 23:33:56 +0000

    Alterando algumas coisas

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../lib/database";
+import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query({ text: "SELECT 1 + 1;" });
-  console.log(result);
+  const result = await database.query({ text: "SELECT 1 + 1 as sum" });
+  console.log(result.rows);
   response.status(200).json({ chave: "Está tudo correto" });
 }
 
