341a94ed7b3f1d6f7c8dbf7d01f2c490fd812523
Invencible-Maximilian <Maximilianwachter@outlook.com>
2026-02-10 21:41:21 +0000

    move .ent to env.development

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,11 @@
+
+function status(request, response) {
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query({ text: "SELECT 1 + 1 as sum" });
+  const result = await database.query("SELECT 1 + 1 as sum");
   console.log(result.rows);
-  response.status(200).json({ chave: "Está tudo correto" });
+  response.status(200).json({ chave: "são acima da média", sum: result.rows[0].sum });
+  }
 }
-
 export default status;
